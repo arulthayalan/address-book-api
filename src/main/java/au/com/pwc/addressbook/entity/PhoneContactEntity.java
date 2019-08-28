d5e760fe906894c43cedd5b709aa01214b926f3c
Arul George <arulthayalan@hotmail.com>
2019-08-29 00:54:42 +1000

    multiple addressbooks

---
+++ src/main/java/au/com/pwc/addressbook/entity/PhoneContactEntity.java
@@ -0,0 +1,61 @@
+package au.com.pwc.addressbook.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "phone_contact")
+public class PhoneContactEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+
+    @Column(name = "address_book_name")
+    private String addressBookName;
+
+    @Column
+    private String name;
+
+
+    @Column
+    private String mobileNumber;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getAddressBookName() {
+        return addressBookName;
+    }
+
+    public void setAddressBookName(String addressBookName) {
+        this.addressBookName = addressBookName;
+    }
+}
---
+++ src/main/java/au/com/pwc/addressbook/model/AddressBook.java
@@ -0,0 +1,38 @@
+package au.com.pwc.addressbook.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@ApiModel("Phone address book ")
+public class AddressBook {
+
+    @ApiModelProperty(value = "Address book name. Name must be unique", required = true)
+    @NotNull
+    private String name;
+
+    @ApiModelProperty(value = "Address book contact", required = true)
+    @Valid
+    @Size(min = 1)
+    private List<PhoneContact> phoneContacts;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<PhoneContact> getPhoneContacts() {
+        return phoneContacts;
+    }
+
+    public void setPhoneContacts(List<PhoneContact> phoneContacts) {
+        this.phoneContacts = phoneContacts;
+    }
+}
