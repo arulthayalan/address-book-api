62c22c8199e6356e8f89f2e8820e947e967d9c39
Arul George <arulthayalan@hotmail.com>
2019-08-29 20:02:50 +1000

    Revert "multiple addressbooks (#1)"
    This reverts commit c07ebcd79187d30f4f9a6edb9cd38844e87661cc.

--- src/main/java/au/com/pwc/addressbook/respository/AddressBookRepository.java
+++
@@ -1,9 +0,0 @@
-package au.com.pwc.addressbook.respository;
-
-import au.com.pwc.addressbook.entity.AddressBookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressBookRepository extends JpaRepository<AddressBookEntity, String> {
-
-
-}
